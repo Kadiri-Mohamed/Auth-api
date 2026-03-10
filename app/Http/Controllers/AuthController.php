7673a43753f149d128641c3c7663b79134c41f7e
Kadiri <kadiri.moohamed@gmail.com>
2026-03-10 12:03:31 +0000

    feat: add AuthController with signUp method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function signUp(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed'
+        ]);
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return response()->json([
+            'message' => 'Account created successfully',
+            'user' => $user
+        ], 201);
+
+    }
+    public function signIn(Request $request)
+    {
+
+    }
+
+    public function signOut()
+    {
+
+    }
+}
