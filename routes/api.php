ce38fed9957943c92d070d4333ce89a58df42ad0
Kadiri <kadiri.moohamed@gmail.com>
2026-03-10 14:05:37 +0000

    feat: add auth routes for signUp, signIn, signOut

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('signUp', [AuthController::class, 'signUp']);
+Route::post('signIn', [AuthController::class, 'signIn']);
+Route::middleware('auth:api')->group(function () {
+
+    Route::post('signOut', [AuthController::class, 'signOut']);
+
+});
+
+
+
